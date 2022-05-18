90f3beee83db1ef862a11134dd0bdb44d22a2e50
adanabdillahi <adanabdillahi30@gmail.com>
2022-05-18 21:20:21 +0100

    dataloader included

--- src/main/java/com/bnta/carsales/components/DataLoader.java
+++ src/main/java/com/bnta/carsales/components/DataLoader.java
@@ -1,4 +1,23 @@
 package com.bnta.carsales.components;
 
-public class DataLoader {
+import com.bnta.carsales.repositories.CarRepository;
+import com.bnta.carsales.repositories.DealershipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    CarRepository carRepository;
+
+    @Autowired
+    DealershipRepository dealershipRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception{
+
+    }
 }
