d282e16b42003ed96d5d6c07ec3858409a35f045
adanabdillahi <adanabdillahi30@gmail.com>
2022-05-18 17:19:35 +0100

    SpringInitializer

---
+++ src/main/java/com/bnta/carsales/CarsalesApplication.java
@@ -0,0 +1,14 @@
+package com.bnta.carsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsalesApplication {
+	ss
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsalesApplication.class, args);
+	}
+
+}
